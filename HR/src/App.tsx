4495933bb00c16164558c822ab51a98cda45ec95
harshrajput177 <harshrajput30411@gmail.com>
2026-04-08 13:37:40 +0530

    u

--- HR/src/App.tsx
+++ HR/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { 
   Search,
